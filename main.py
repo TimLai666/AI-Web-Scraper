714deb6512570d8227f9e445eef1c7c7ae4f3969
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-16 22:01:36 +0800

    新增網頁爬蟲功能，擴充爬取邏輯，包含提取、清理和分割網頁內容

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content
+)
 
 st.title("AI Web Scraper")
 url = st.text_input("輸入網址")
 
 if st.button("開始爬取"):
     st.write("正在爬取網頁資料...")
-    html = scrape_website(url)
-    st.write(html)
+    html: str = scrape_website(url)
+    body: str = extract_body_content(html)
+    cleaned_content: str = clean_body_content(body)
+    st.session_state.dom_content = split_dom_content(cleaned_content)
+
+    with st.expander("爬取結果", expanded=True):
+        st.text_area(f"{url} 的爬取結果：", cleaned_content, height=300)
--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 import time
 
-def scrape_website(website):
+def scrape_website(website: str) -> str:
     print("正在啟動瀏覽器...")
     
     options = webdriver.ChromeOptions()
@@ -16,7 +17,27 @@ def scrape_website(website):
         driver.get(website)
         print("成功連線到網站")
         time.sleep(10)
-        html = driver.page_source
+        html: str = driver.page_source
         return html
     finally:
         driver.quit()
+
+def extract_body_content(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    body = soup.find("body")
+    return body.prettify() if body else ""
+
+def clean_body_content(body: str) -> str:
+    soup = BeautifulSoup(body, "html.parser")
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content: str = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(stripped_line for line in cleaned_content.splitlines() if (stripped_line := line.strip()))
+
+    return cleaned_content
+
+def split_dom_content(dom_content: str, max_length=6000):
+    return [
+        dom_content[i:i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
